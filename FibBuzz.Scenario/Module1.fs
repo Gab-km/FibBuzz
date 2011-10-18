a343f9496b31f116793e6c893601eebfbf03e65b
Gab_km <the25thcromosome@gmail.com>
2011-10-18 23:08:15 +0900

    Added FibBuzz.Scenario project and wrote the first test.

---
+++ FibBuzz.Scenario/Module1.fs
@@ -0,0 +1,11 @@
+﻿module FibBuzz.Scenario
+
+open NaturalSpec
+open FibBuzz.Main
+
+[<Scenario>]
+let ``When fibonacci takes 0 then it returns [1].`` () =
+    Given 0
+    |> When fibonacchi
+    |> It should equal [1]
+    |> Verify
