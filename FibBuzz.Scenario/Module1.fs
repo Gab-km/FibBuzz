9349753f71de0c0e6f786a8f0f57a7d8a5ae9e63
Gab_km <the25thcromosome@gmail.com>
2011-10-18 23:56:33 +0900

    Wrote fibonacci as it works.

--- FibBuzz.Scenario/Module1.fs
+++ FibBuzz.Scenario/Module1.fs
@@ -17,3 +17,10 @@ let ``When fibonacci takes 1 then it returns [1; 1].`` () =
     |> When fibonacci
     |> It should equal [1; 1]
     |> Verify
+
+[<Scenario>]
+let ``When fibonacci takes 2 then it returns [1; 1; 2].`` () =
+    Given 2
+    |> When fibonacci
+    |> It should equal [1; 1; 2]
+    |> Verify
--- FibBuzz/Program.fs
+++ FibBuzz/Program.fs
@@ -1,9 +1,16 @@
 ﻿module FibBuzz.Main
 
 let fibonacci n =
-    let rec fib n xs =
-        if n = 0 then
-            xs
+    let rec fib n =
+        if n > 1 then
+            (fib (n - 1)) + (fib (n - 2))
+        elif n = 1 || n = 0 then
+            1
         else
-            fib (n - 1) (1::xs)
-    fib n [1]
+            0
+    let rec fibList n xs =
+        if n = 0 then
+            ((fib n)::xs)
+        else
+            fibList (n - 1) ((fib n)::xs)
+    fibList n []
